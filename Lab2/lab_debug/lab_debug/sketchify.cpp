6fcbc62ba7eafb55a369bca5ebd038f5e59ae0b0
Eemil Kulmala <eemil.kulmala@hotmail.com>
2024-03-22 23:13:43 +0200

    Lab3 done

--- Lab2/lab_debug/lab_debug/sketchify.cpp
+++ Lab2/lab_debug/lab_debug/sketchify.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib>
 #include "png.h"
+#include <iostream>
+using namespace std;
 
 //
 // sets up the output image
@@ -13,51 +15,55 @@ PNG * setupOutput(int w, int h)
 // Returns my favorite color
 RGBAPixel * myFavoriteColor(int intensity)
 {
-	RGBAPixel color;
-	color.red   = 0;
-	color.green = intensity/2;
-	color.blue  = intensity;
-	return &color;
+	RGBAPixel * color = new RGBAPixel();
+	color->red   = 0;
+	color->green = intensity/2;
+	color->blue  = intensity;
+	return color;
 }
 
 
 void sketchify()
 {
 	// Load in.png
-	PNG * original;
-
+	PNG * original = new PNG();
 	original->readFromFile("in.png");
+
 	int width  = original->width();
 	int height = original->height();
 
 	// Create out.png
 	PNG * output;
-	setupOutput(width, height);	
+	output = setupOutput(width, height);	
 
-	// Loud our favorite color to color the outline
-	RGBAPixel * myPixel = myFavoriteColor(192);
+	output->writeToFile("out.png"); // TODO REMOVE
+	
+	// Load our favorite color to color the outline
+	RGBAPixel *myPixel = myFavoriteColor(192);
 
 	// Go over the whole image, and if a pixel differs from that to its upper
 	// left, color it my favorite color in the output
-	for (int y = 1; 0 < y < height; y++)
+	for (int y = 1; y < height; y++)
 	{
-		for (int x = 1; 0 < x < width; x++)
+		for (int x = 1; x < width; x++)
 		{
 			// Calculate the pixel difference
 			RGBAPixel * prev = (*original)(x-1, y-1);
 			RGBAPixel * curr = (*original)(x  , y  );
+			
 			int diff = abs(curr->red   - prev->red  ) +
 					   abs(curr->green - prev->green) +
 					   abs(curr->blue  - prev->blue );
-
 			// If the pixel is an edge pixel,
 			// color the output pixel with my favorite color
-			RGBAPixel * currOutPixel = (*output)(x,y);
+
+			RGBAPixel * currOutPixel = (*output)(x, y);
 			if (diff > 100)
-				currOutPixel = myPixel;
+			{
+				*currOutPixel = *myPixel;
+			}
 		}
 	}
-	
 	// Save the output file
 	output->writeToFile("out.png");
 
